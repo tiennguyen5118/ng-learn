f74a16be3c60b715916c34adbc300c82cdc17646
Tien Nguyen <tiennguyen5118@gmail.com>
2024-08-16 09:44:39 +0700

    init page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
+import { GetStartedComponent } from './pages/get-started/get-started.component';
 import { Routes } from '@angular/router';
+import { MaterialComponent } from './pages/material/material.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'get-started',
+    component: GetStartedComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'material',
+    component: MaterialComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
